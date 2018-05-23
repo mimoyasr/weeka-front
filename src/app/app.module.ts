0811170476be833a70f6b86bb7007684ac37fafd
Raneen <raneen.medhat@gmail.com>
2018-05-23 14:40:53 +0200

    raneen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,41 +2,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
 //App component
+
 import { AppComponent } from './app.component';
-import { HomeGuestComponent } from './home-guest/home-guest.component';
-import { FooterComponent } from './footer/footer.component';
-import { MealCardComponent } from './meal-card/meal-card.component';
-import { StarRatingModule } from 'angular-star-rating';
-
-//services
-import { QueryService } from './query.service';
-
-const appRoutes: Routes = [
-  { path: '',component:HomeGuestComponent },
-  
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeGuestComponent,
-    FooterComponent,
-    MealCardComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot(),
-    RouterModule.forRoot(appRoutes),
-    StarRatingModule.forRoot(),
-    HttpClientModule,
-    FormsModule
+    NgbModule.forRoot()
   ],
-  providers: [QueryService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
