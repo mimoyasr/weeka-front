98ec817db78159c02499e7b18370c0069946bdd5
Raneen <raneen.medhat@gmail.com>
2018-05-23 14:21:36 +0200

    meal card creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-//App component
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+//App component
 import { AppComponent } from './app.component';
+import { HomeGuestComponent } from './home-guest/home-guest.component';
+import { FooterComponent } from './footer/footer.component';
+import { MealCardComponent } from './meal-card/meal-card.component';
+import { StarRatingModule } from 'angular-star-rating';
+
+//services
+import { QueryService } from './query.service';
+
+const appRoutes: Routes = [
+  { path: '',component:HomeGuestComponent },
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeGuestComponent,
+    FooterComponent,
+    MealCardComponent,
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    StarRatingModule.forRoot(),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [QueryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
