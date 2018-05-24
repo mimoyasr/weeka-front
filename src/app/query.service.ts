41a3c4a56a1c87c3198d0a766cfa5626c7dd5008
Raneen <raneen.medhat@gmail.com>
2018-05-24 11:52:00 +0200

    Update home page

---
+++ src/app/query.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QueryService {
+
+  constructor(private http:HttpClient) {  
+  }
+  
+  getData(path: string):Observable<any>{
+    return this.http.get(path)
+  }
+}
