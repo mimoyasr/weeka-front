3ed3d477829053cf01ce8343f1ba180c08a9fd43
Dina Khaled <dina.elghndour@gmail.com>
2018-06-20 16:00:38 -0700

    notification fixed

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 //service
 import { TransferDataService } from '../transfer-data.service';
 import { element } from 'protractor';
+import { HttpHeaders } from '@angular/common/http';
 import { QueryService } from '../query.service';
 
 @Component({
@@ -15,27 +16,47 @@ export class NotificationComponent implements OnInit {
   confirmedMeals: Array<object>;
   totalOneMeal: number;
   totalAllMeals: number;
-  constructor(private transfer: TransferDataService, private query: QueryService) {
+  homeImg:Array<object>;  
+  
+  constructor(private transfer: TransferDataService, private Q: QueryService) {
 
     this.transfer.cast.subscribe(product => this.allMealCart = product);
-    this.cookerName = 'محمد على';
+    this.cookerName = '';
     this.confirmedMeals = [];
     this.totalAllMeals = 0;
-    this.totalOneMeal = 0;
-    // this.chefMeals();
-    this.getMealsData();
+    this.getMealData();
+    this.calcTotalOneMeal();
+    this.homeImg = [];        
+
   }
 
   ngOnInit() {
   }
 
+  getMealData(): void {
+    let path: string = 'http://weeka.herokuapp.com/api/home';
+    let tokenUser = localStorage.getItem('token');
+    this.Q.getData2(path, {
+      headers: new HttpHeaders({ 'Authorization': `Bearer ${tokenUser}` })
+    }).subscribe(
+      res => {
+        this.cookerName = res.meals[0].chef_name;
+        this.chefMeals();
+        this.getHomeImg();
+
+      },
+      err => { console.log(err) }
+    );
+  }
+
   // check chef meals
   chefMeals(): void {
     this.allMealCart.forEach(element => {
-      if (element["cookerName"] == this.cookerName) {
+      if (element["chef_name"] == this.cookerName) {
         this.confirmedMeals.push(element);
       }
     })
+    console.log(this.confirmedMeals);
   }
 
   // calc total price for every meal
@@ -50,7 +71,7 @@ export class NotificationComponent implements OnInit {
   //confirm order
   confirmOrder(id): void {
     this.confirmedMeals.forEach(element => {
-      if (element['id'] == id) {
+      if (element['meal_id'] == id) {
         let productIndex = this.confirmedMeals.indexOf(element);
         this.confirmedMeals.splice(productIndex, 1);
 
@@ -58,14 +79,11 @@ export class NotificationComponent implements OnInit {
     })
   }
 
-  getMealsData() {
-    let path: string = "../../assets/chefs-notifications.json";
-    this.query.getData(path).subscribe(
+  getHomeImg(): void {
+    let path: string = "./assets/images-home.json";
+    this.Q.getData(path).subscribe(
       res => {
-        this.confirmedMeals = res;
-        console.log(this.confirmedMeals);
-        this.calcTotalOneMeal();
-        // console.log(element['totalOneMeal']);
+        this.homeImg = res;
       },
       err => { console.log(err) }
     );
